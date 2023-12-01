31f8cc3783bd12d71727e060eecb231361385ac0
UsmanovaRenata <usrenata01@mail.ru>
2023-12-01 11:47:27 +0300

    new commit

--- server/main.py
+++ server/main.py
@@ -5,6 +5,7 @@ from model import db
 from routes import app
 
 if __name__ == "__main__":
+    app.secret_key = "key"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:{database_settings()}"
     db.init_app(app)
     with app.app_context():
--- server/routes.py
+++ server/routes.py
@@ -1,69 +1,59 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort, make_response
+from flask_login import LoginManager, login_user, login_required
 from logger import logger
-from config import check_credentials, save_to_config
+from config import check_credentials, User
 from model import db, Student
 import base64
 import datetime
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
 
 @app.route('/login', methods=['GET', 'POST'])
-def authorization(newUser=False):
+def authorization():
     request_data = request.json
     username = request_data.get('username')
     password = request_data.get('password')
     if username and password:
         if check_credentials(username, password):
-            if(newUser):
-                responseData = {"message": 403}
-                logger.info("the user already exists")
-            else:
-                responseData = {"message": 200}
-                logger.info("authorization was successful")
+            user = User(username)
+            login_user(user)
+            logger.info("authorization was successful")
+            return jsonify({"message": 200}), 200
         else:
-            if(newUser):
-                save_to_config(username, password)
-                responseData = {"message": 200}
-                logger.info("user added successfully")
-            else:
-                responseData = {"message": 403}
-                logger.warning("the user does not exist")
+            logger.warning("the user does not exist")
+            return jsonify({"message": 403}), 403
     else:
-        responseData = {"message": 400}
         logger.warning("incorrect data")
+        return jsonify({"message": 404}), 404
 
-    return jsonify(responseData)
 
-@app.route('/add_user', methods=['GET', 'POST'])
-def add_user():
-    return authorization(True)
 
 @app.route('/all', methods=['GET'])
+@login_required
 def all_students():
     students = Student.query.all()
     students_json = []
-
     for student in students:
-        if student.photo:
-            photo = base64.b64encode(student.photo).decode()
-        else:
-            photo = None
-        if student.date:
-            date = datetime.date.isoformat(student.date)
-        else:
-            date = None
+        photo = base64.b64encode(student.photo).decode()
+        date = datetime.date.isoformat(student.date)
         student_data = student.serialize()
         student_data['photo'] = photo
         student_data['date'] = date
         students_json.append(student_data)
     if len(students_json) != 0:
         logger.info("all data has been sent")
-        return jsonify(message=200, students=students_json)
+        return jsonify(message=200, students=students_json), 200
     else:
         logger.critical("error sending data")
-        return jsonify({"message": 500})
+        return jsonify({"message": 500}), 500
 
 @app.route('/change_data', methods=['POST'])
+@login_required
 def change_data():
     changed_data = request.json
     data_id = changed_data.get('id')
@@ -84,13 +74,14 @@ def change_data():
         db.session.commit()
 
         logger.info("data changed successfully")
-        return jsonify({"message": 200})
+        return jsonify({"message": 200}), 200
     else:
         logger.critical("data modification error")
-        return jsonify({"message": 500})
+        return jsonify({"message": 500}), 500
 
 
 @app.route('/add_student', methods=['POST'])
+@login_required
 def add_student():
     new_student_data = request.json
     name = new_student_data.get('name_student')
@@ -120,12 +111,13 @@ def add_student():
         }
 
         logger.info("new student successfully added")
-        return jsonify(response_data)
+        return jsonify(response_data), 200
     else:
         logger.critical("error adding a new student")
-        return jsonify({"message": 500})
+        return jsonify({"message": 500}), 500
 
 @app.route('/delete_student', methods=['POST'])
+@login_required
 def delete_student():
     delete_student_data = request.json
     id = delete_student_data.get('id')
@@ -134,7 +126,7 @@ def delete_student():
         db.session.delete(student)
         db.session.commit()
         logger.info("student successfully removed")
-        return jsonify({"message": 200})
+        return jsonify({"message": 200}), 200
     logger.critical("student deletion error")
-    return jsonify({"message": 500})
+    return jsonify({"message": 500}), 500
 
